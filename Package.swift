e4152f03c54aab94d36bd31c6fc9bd1cd53b734f
CNL <93285315+codeninelabs@users.noreply.github.com>
2024-12-15 17:30:49 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,19 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "CNLToaster",
+    name: "cnl_toaster",
     platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "CNLToaster",
-            targets: ["CNLToaster"]),
+            name: "cnl_toaster",
+            targets: ["cnl_toaster"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CNLToaster"),
+            name: "cnl_toaster"),
 
     ]
 )
